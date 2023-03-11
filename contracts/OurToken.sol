b5f94ac9b0090c37d29ee30cfe98d85ce5dd07e6
Yana Gupta <yanagupta897@gmail.com>
2023-03-11 23:07:32 +0530

    Formatted Code

--- contracts/OurToken.sol
+++ contracts/OurToken.sol
@@ -1,13 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-
 // Imported openzeppelin to use their token
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract OurToken is ERC20 {
-    
     constructor(uint256 initialSupply) ERC20("OurToken", "OTC") {
         _mint(msg.sender, initialSupply);
     }
